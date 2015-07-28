ff9a32b479cd0b26c819c1f95768f7cd544464b5
Dakota Benjamin <dakota.benjamin@gmail.com>
2015-07-28 15:50:25 -0400

    Add user registration and login
    This commit adds functionality for user registration and login. TODO: itsdangerous integration for email authentication.

--- app/main/views.py
+++ app/main/views.py
@@ -3,8 +3,8 @@ from flask import render_template, session, redirect, url_for
 from . import main
 #from .forms import NameForm
 from .. import db
-#from ..models import User
-
+from ..models import User
+from flask.ext.login import login_required
 # Home Page
 @main.route('/', methods=['GET', 'POST'])
 def index():
@@ -12,5 +12,6 @@ def index():
 
 # User Page
 @main.route('/user/<name>')
+@login_required
 def user(name):
 	return render_template('user.html',name=name)
